988ac677bdbad6efaae93817911dbbbb0976f962
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-17 09:54:36 +0100

    added random colour gen

---
+++ Assets/Scripts/RandomGeneration/RandomColour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColour : MonoBehaviour
+{
+
+    [SerializeField] List<ObjectColour> colors = new List<ObjectColour>();
+    void Start()
+    {
+        
+    }
+}
