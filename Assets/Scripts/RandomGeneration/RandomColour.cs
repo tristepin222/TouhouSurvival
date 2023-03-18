22c1e7195852650160704460d82650edc67c7e7a
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-18 18:15:24 +0100

    added color gen

--- Assets/Scripts/RandomGeneration/RandomColour.cs
+++ Assets/Scripts/RandomGeneration/RandomColour.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class RandomColour : MonoBehaviour
 {
 
-    [SerializeField] List<ObjectColour> colors = new List<ObjectColour>();
+    [SerializeField] List<ObjectColour> colours = new List<ObjectColour>();
     void Start()
     {
-        
+        SetColour();
     }
+
+    public List<ObjectColour> Colours
+    {
+        get { return colours; }
+        set { colours.AddRange(value); }
+    }
+
+    private void SetColour()
+    {
+        foreach (ObjectColour colour in colours)
+        {
+            SpriteRenderer spriteRenderer = gameObject.transform.Find(colour.objectName).GetComponent<SpriteRenderer>();
+            spriteRenderer.color = colour.color;
+        }
+    }
+
 }
