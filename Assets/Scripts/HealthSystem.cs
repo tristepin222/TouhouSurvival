f7d98a6c0b8d89c9f00ac78d0edb41fd0f547040
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-20 11:59:45 +0100

    rename health

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealhSystem : MonoBehaviour, IDamageable
+public class HealthSystem : MonoBehaviour, IDamageable
 {
     [SerializeField] float baseHealth;
 
@@ -24,13 +24,5 @@ public class HealhSystem : MonoBehaviour, IDamageable
     private void Die()
     {
         Destroy(this);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Player")
-        {
-            Damage(1f);
-        }
     }
 }
