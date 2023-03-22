3b671d7debc6eb1d04e7b858136fa08500048773
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-22 14:05:29 +0100

    added animators and VFX to effect player

--- Assets/Scripts/Misc/EffectPlayer.cs
+++ Assets/Scripts/Misc/EffectPlayer.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.VFX;
 
 public class EffectPlayer : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] protected Animator globalAnimator;
+    [SerializeField] protected Animator playerAnimator;
+    [SerializeField] protected VisualEffect playerVisualEffect;
     public void StopAllAIs()
     {
         AIPath[] gameObjects = FindObjectsOfType<AIPath>();
