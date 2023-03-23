927966b3732d7310d9bfa951abb5f3116073f9e2
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-23 12:59:08 +0100

    added structs for random deaths

---
+++ Assets/Scripts/Structs/Death.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class Death
+{
+    string eventName;
+    string animationName;
+}
+
