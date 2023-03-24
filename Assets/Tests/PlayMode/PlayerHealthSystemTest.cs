078a3825280c736eeb8e2c1fa3e66393194d017c
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-24 17:08:48 +0100

    test: added test to PlayerHealthSystem

---
+++ Assets/Tests/PlayMode/PlayerHealthSystemTest.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerHealthSystemTest
+{
+    GameObject gameObject;
+    PlayerHealthSystem healthSystem;
+    [SetUp]
+    public void SetUpTest()
+    {
+        gameObject = new GameObject();
+        healthSystem = gameObject.AddComponent<PlayerHealthSystem>();
+    }
+
+    [Test]
+    public void SetHealthToOneDamageOneReturnsOne()
+    {
+        float Whenhealth = healthSystem.Health = 2;
+
+        healthSystem.Damage(1);
+
+        Whenhealth--;
+
+        Assert.AreEqual(Whenhealth, healthSystem.Health);
+    }
+
+    [UnityTest]
+    public IEnumerator DamageToZeroDoesNotDestroyGameObject()
+    {
+        GameObject gameObjectToDestroy = new GameObject();
+        healthSystem = gameObjectToDestroy.AddComponent<PlayerHealthSystem>();
+        float Whenhealth = healthSystem.Health = 2;
+
+        healthSystem.Damage(2);
+
+        yield return new WaitForEndOfFrame();
+
+        //unity workaround even if object is destroyed, it's not "null"
+        Assert.IsTrue(gameObjectToDestroy != null);
+    }
+}
