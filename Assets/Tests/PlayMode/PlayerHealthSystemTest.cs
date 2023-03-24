6eea08ef2322bf9361e94116630a30713f060fad
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-24 17:46:42 +0100

    test: added asserts on throws

--- Assets/Tests/PlayMode/PlayerHealthSystemTest.cs
+++ Assets/Tests/PlayMode/PlayerHealthSystemTest.cs
@@ -7,38 +7,63 @@ using UnityEngine.TestTools;
 public class PlayerHealthSystemTest
 {
     GameObject gameObject;
-    PlayerHealthSystem healthSystem;
+    PlayerHealthSystem playerHealthSystem;
+    EffectPlayer effectPlayer;
+    Rigidbody2D rigidbody2D;
+    FightSystem fightSystem;
     [SetUp]
     public void SetUpTest()
     {
         gameObject = new GameObject();
-        healthSystem = gameObject.AddComponent<PlayerHealthSystem>();
+        playerHealthSystem = gameObject.AddComponent<PlayerHealthSystem>();
+        effectPlayer = gameObject.AddComponent<EffectPlayer>();
+        rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        fightSystem = gameObject.AddComponent<FightSystem>();
+        playerHealthSystem.effectPlayer = effectPlayer;
+        playerHealthSystem.rigidbody2D = rigidbody2D;
+        playerHealthSystem.fightSystem = fightSystem;
     }
 
     [Test]
     public void SetHealthToOneDamageOneReturnsOne()
     {
-        float Whenhealth = healthSystem.Health = 2;
+        float Whenhealth = playerHealthSystem.Health = 2;
 
-        healthSystem.Damage(1);
+        playerHealthSystem.Damage(1);
 
         Whenhealth--;
 
-        Assert.AreEqual(Whenhealth, healthSystem.Health);
+        Assert.AreEqual(Whenhealth, playerHealthSystem.Health);
     }
 
     [UnityTest]
     public IEnumerator DamageToZeroDoesNotDestroyGameObject()
     {
-        GameObject gameObjectToDestroy = new GameObject();
-        healthSystem = gameObjectToDestroy.AddComponent<PlayerHealthSystem>();
-        float Whenhealth = healthSystem.Health = 2;
+        float Whenhealth = playerHealthSystem.Health = 2;
 
-        healthSystem.Damage(2);
+        playerHealthSystem.Damage(2);
 
         yield return new WaitForEndOfFrame();
 
         //unity workaround even if object is destroyed, it's not "null"
-        Assert.IsTrue(gameObjectToDestroy != null);
+        Assert.IsTrue(gameObject != null);
+    }
+    [Test]
+    public void NullEffectPlayerThrowsException()
+    {
+        playerHealthSystem.effectPlayer = null;
+        Assert.Throws<EmptyException.EmptyEffectPlayerException>(() => playerHealthSystem.Damage(2));
+    }
+    [Test]
+    public void NullEffectRigidbody2DException()
+    {
+        playerHealthSystem.rigidbody2D = null;
+        Assert.Throws<EmptyException.EmptyRigidbody2DException>(() => playerHealthSystem.Damage(2));
+    }
+    [Test]
+    public void NullEffectFightSystemException()
+    {
+        playerHealthSystem.fightSystem = null;
+        Assert.Throws<EmptyException.EmptyFightSystemException>(() => playerHealthSystem.Damage(2));
     }
 }
