d0f1b609ddaef77d135fb0d9ed00abf81b83ecc7
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-01 20:31:39 +0200

    feat: added loot drop system

---
+++ Assets/Scripts/Misc/LootSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootSystem : MonoBehaviour
+{
+    [SerializeField] GameObject loot;
+    // Start is called before the first frame update
+
+    private void OnDestroy()
+    {
+        GameObject currentLoot = Instantiate(loot);
+        currentLoot.transform.position = transform.position;
+    }
+}
