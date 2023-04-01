42dc51060ef86f1705b0b562993c6b2507090b14
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-01 23:41:39 +0200

    feat: update level system

--- Assets/Scripts/Misc/LevelSystem.cs
+++ Assets/Scripts/Misc/LevelSystem.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LevelSystem : MonoBehaviour
 {
+    [SerializeField] Slider XPSlider;
+    [SerializeField] float MaxXPAmount;
+    private float xp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        XPSlider.maxValue = MaxXPAmount;
+    }
+    public void addXP(float amount)
+    {
+        xp += amount;
+        XPSlider.value = xp;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
