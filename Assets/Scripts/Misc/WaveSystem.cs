59062325f92e25e17c956f10fa0aac96dd4f4335
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-03 13:08:14 +0200

    feat: added timed shop tp

--- Assets/Scripts/Misc/WaveSystem.cs
+++ Assets/Scripts/Misc/WaveSystem.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 public class WaveSystem : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI waveValueText;
     [SerializeField] TextMeshProUGUI timeValueText;
     [SerializeField] float baseTimeValue;
     [SerializeField] float baseWaveValue;
+    [SerializeField] string sceneName;
+    private float timeValue;
+    private float waveValue;
     // Start is called before the first frame update
     void Start()
     {
+        timeValue = baseTimeValue;
+        waveValue = baseWaveValue;
         InvokeRepeating("setTime", 0, 1.0f);
     }
 
     private void setTime()
     {
-        baseTimeValue--;
-        timeValueText.text = baseTimeValue.ToString();
+        timeValue--;
+        timeValueText.text = timeValue.ToString();
+        if(timeValue == 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
