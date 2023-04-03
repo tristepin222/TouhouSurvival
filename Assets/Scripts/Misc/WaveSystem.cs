4ddd1d0461279d2b78fa14ac20535677c8f19aea
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-03 10:42:38 +0200

    feat: made a timer

--- Assets/Scripts/Misc/WaveSystem.cs
+++ Assets/Scripts/Misc/WaveSystem.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class WaveSystem : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI waveValueText;
+    [SerializeField] TextMeshProUGUI timeValueText;
+    [SerializeField] float baseTimeValue;
+    [SerializeField] float baseWaveValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("setTime", 0, 1.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void setTime()
     {
-        
+        baseTimeValue--;
+        timeValueText.text = baseTimeValue.ToString();
     }
 }
