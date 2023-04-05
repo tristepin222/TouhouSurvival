f0030f570db7b72378815432d7aba0aa368a608d
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-05 13:39:47 +0200

    feat: added loot to xp

---
+++ Assets/Scripts/Misc/Loot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    private bool canMoveTowards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.rotation = new Quaternion(0, 0, Random.rotation.z, Random.rotation.w); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMoveTowards)
+        {
+            var step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, step);
+        }
+    }
+
+    public void MoveTowards()
+    {
+        canMoveTowards = true;
+    }
+}
