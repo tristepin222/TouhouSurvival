93186efd05d751166627ba72dd6f4d6e39c5fac3
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-08 10:30:42 +0200

    feat: added scriptable object for upgrades

---
+++ Assets/Scripts/ScriptableObjects/UpgradeScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/cropScriptableObject")]
+public class UpgradeScriptableObject
+{
+    public string UpgradeName;
+    public float UpgradeValue;
+    public string UpgradeDescription;
+}
