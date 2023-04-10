5cb831e4fc4ea0a510343a5f42e852bfcfe63245
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-10 13:58:42 +0200

    feat: added xp slider curve

--- Assets/Scripts/Misc/LevelSystem.cs
+++ Assets/Scripts/Misc/LevelSystem.cs
@@ -7,15 +7,27 @@ public class LevelSystem : MonoBehaviour
     [SerializeField] Slider XPSlider;
     [SerializeField] float MaxXPAmount;
     private float xp;
+
+    private const float RATIO = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        XPSlider.maxValue = MaxXPAmount;
+        CalculateNewMaxAmount();
     }
     public void addXP(float amount)
     {
+        if (xp >= MaxXPAmount)
+        {
+            xp = 0;
+            GlobalController.Instance.level++;
+            CalculateNewMaxAmount();
+        }
         xp += amount;
         XPSlider.value = xp;
+    }
+    private void CalculateNewMaxAmount()
+    {
+        XPSlider.maxValue = MaxXPAmount * (GlobalController.Instance.level + 1 * RATIO);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
