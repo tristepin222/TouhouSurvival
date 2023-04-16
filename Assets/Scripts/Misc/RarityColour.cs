478bcb378cfecc074d93475bb6aab83523453e10
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-17 00:03:37 +0200

    feat: added rarity colors

---
+++ Assets/Scripts/Misc/RarityColour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class RarityColour
+{
+    public string Name;
+    public Color imageColor;
+    public Color textColor;
+}
