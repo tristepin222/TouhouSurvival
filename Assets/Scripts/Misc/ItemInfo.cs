045fcabbfc1246b2a5cb377ec97a380f0ade3527
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-04-24 22:44:54 +0200

    feat: added item info

---
+++ Assets/Scripts/Misc/ItemInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class ItemInfo
+{
+    public string damageInfo;
+    public float damageValue;
+}
