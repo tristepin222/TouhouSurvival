6634e7074177e5f8d6dc511763fcda83834e80c9
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-05-05 21:51:01 +0200

    feat: created  ui elements struct

---
+++ Assets/Scripts/Structs/UIShopElement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+[Serializable]
+public class UIShopElement 
+{
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI subName;
+    public TextMeshProUGUI moneyAmount;
+    public TextMeshProUGUI stats;
+    public Image imageSprite;
+    public Image backGround;
+}
