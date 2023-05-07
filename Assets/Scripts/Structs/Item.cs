dd7f32c84e9f415e3c5ac7374a76b80f4e0af066
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-05-07 16:17:29 +0200

    feat: added Item struct

---
+++ Assets/Scripts/Structs/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Item
+{
+    public enum ItemType
+    {
+        Coin,
+        XPOrb,
+    }
+}
