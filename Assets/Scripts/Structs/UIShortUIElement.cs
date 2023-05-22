0b2eccbaaf012c052d12cd29da4bf9c73eb9c5e1
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-05-22 13:39:26 +0200

    feat: added short ui elements

---
+++ Assets/Scripts/Structs/UIShortUIElement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+[Serializable]
+public class UIShortUIElement
+{
+    public TextMeshProUGUI name;
+    public Image parent;
+    public Image backGround;
+}
