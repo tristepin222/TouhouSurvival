49c28370232510e2fe5841bf033778cb6530f7ff
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-05-22 16:29:21 +0200

    feat: added on mouse over

--- Assets/Scripts/UI/UIItem.cs
+++ Assets/Scripts/UI/UIItem.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class UIItem : MonoBehaviour
+public class UIItem : MonoBehaviour, IPointerEnterHandler
 {
     public ItemScriptableObject ItemScriptableObject;
+    public GameObject canvas;
+    public GameObject template;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Instantiate(template, canvas.transform);
+        template.GetComponent<RectTransform>().anchoredPosition = Input.mousePosition / canvas.transform.localScale.x;
+    }
 }
