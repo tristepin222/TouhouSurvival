5f273aa4630f140e41ae916ccd46385c8596a8fa
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-05-23 13:57:35 +0200

    Update UIOnHoverItem.cs

--- Assets/Scripts/UI/UIOnHoverItem.cs
+++ Assets/Scripts/UI/UIOnHoverItem.cs
@@ -8,7 +8,10 @@ public class UIOnHoverItem : MonoBehaviour, IPointerExitHandler
 {
     [SerializeField] Image sprite;
     [SerializeField] TextMeshProUGUI textMesh;
+    [SerializeField] TextMeshProUGUI description;
     [SerializeField] GameObject parent;
+    [SerializeField] GameObject button;
+    [SerializeField] bool inGame;
 
     public void OnPointerExit(PointerEventData eventData)
     {
@@ -16,9 +19,14 @@ public class UIOnHoverItem : MonoBehaviour, IPointerExitHandler
         gameObject.SetActive(false);
     }
 
-    public void SetUI(Sprite sprite, string text)
+    public void SetUI(Sprite sprite, string text, string description)
     {
+        if (inGame)
+        {
+            button.SetActive(false);
+        }
         this.sprite.sprite = sprite;
         textMesh.text = text;
+        this.description.text = description;
     }
 }
